a0251a325bf5a1e79d1f57fedcfde099dc40f893
王春祥 <wangchunxiang@tojoy.com>
2021-08-09 17:56:30 +0800

    d

---
+++ src/test/resources/com/github/alphahinex/checkstyle/checkers/NoSwaggerAnnotation.java
@@ -0,0 +1,10 @@
+package com.github.alphahinex.checkstyle.checkers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Not pass check
+ */
+@Controller
+public class NoSwaggerAnnotation {
+}
---
+++ src/test/resources/com/github/alphahinex/checkstyle/checkers/WithNoMethod.java
@@ -0,0 +1,13 @@
+package com.github.alphahinex.checkstyle.checkers;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Pass check
+ */
+@Api(tags = "/no-method")
+@RestController
+public class WithNoMethod {
+
+}
